4ff9547f587fcd02c12043378cc3641a8b960f4d
Anand Sudhanaboina <anand.sudhanaboina@gmail.com>
2019-02-20 10:51:04 +0530

    Solved src/reverseString.js and added isMatrixSquare.js

---
+++ isMatrixSquare.js
@@ -0,0 +1,7 @@
+/*
+input [[2,2],[2,2]]
+output true
+*/
+export const isMatrixSquare = (matrix) => {
+    // TO BE IMPLEMENTED
+};
--- src/reverseString.js
+++ src/reverseString.js
@@ -3,5 +3,5 @@ Input: abcd
 Output: dcba
 */
 export const reverseString = (string) => {
-    // TO IMPLEMENT IN ANOTHER PR
+    return string.split("").reverse().join("");
 };
